e060fb6100724d182eeecea4df25d8b6f20c82a7
Edmund Wohlmuth <edmundwoh@gmail.com>
2023-06-07 11:48:55 -0300

    bug fixes

--- Assets/Scripts/Gameplay/FieldOfView.cs
+++ Assets/Scripts/Gameplay/FieldOfView.cs
@@ -12,6 +12,9 @@ public class FieldOfView : MonoBehaviour
 
     public List<Transform> visableTargets = new List<Transform>();
 
+    public int edgeResolveIterations;
+    public float edgeDistanceThreshold;
+
     public float meshResolution;
     public MeshFilter viewMeshFilter;
     Mesh viewMesh;
@@ -46,21 +49,41 @@ public class FieldOfView : MonoBehaviour
         int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
         float stepAngleSize = viewAngle / stepCount;
         List<Vector3> viewPoints = new List<Vector3>();
+        // find edge
+        ViewCastInfo oldViewCast = new ViewCastInfo();
 
         for (int i = 0; i < stepCount; i++)
         {
             float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
             //Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius, Color.red);
             ViewCastInfo newViewCast = ViewCast(angle);
+            bool edgeDistanceThresholdExceeded = Mathf.Abs(oldViewCast.dist - newViewCast.dist) > edgeDistanceThreshold;
+            if (i > 0)
+            {
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit) && edgeDistanceThresholdExceeded)
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
             viewPoints.Add(newViewCast.point);
+            oldViewCast = newViewCast;
         }
 
-        //create mesh
+        // create mesh
         int vertexCount = viewPoints.Count + 1;
         Vector3[] verticies = new Vector3[vertexCount];
         int[] triangles = new int[(vertexCount - 2) * 3];
 
-        verticies[0] = Vector3.zero; // classic mesh creation
+        verticies[0] = Vector3.zero;
         for (int i = 0; i < vertexCount - 1; i++)
         {
             verticies[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
@@ -77,6 +100,34 @@ public class FieldOfView : MonoBehaviour
         viewMesh.vertices = verticies;
         viewMesh.triangles = triangles;
         viewMesh.RecalculateNormals();
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDistanceThresholdExceeded = Mathf.Abs(minViewCast.dist - newViewCast.dist) > edgeDistanceThreshold;
+            if (newViewCast.hit == minViewCast.hit && !edgeDistanceThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+        return new EdgeInfo(minPoint, maxPoint);
     }
 
     ViewCastInfo ViewCast(float globalAngle)
@@ -124,6 +175,8 @@ public class FieldOfView : MonoBehaviour
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 
+    // STRUCTS
+
     public struct ViewCastInfo
     {
         public bool hit;
@@ -137,6 +190,18 @@ public class FieldOfView : MonoBehaviour
             point = _point;
             dist = _dist;
             angle = _angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 _pointA, Vector3 _pointB)
+        {
+            pointA = _pointA;
+            pointB = _pointB;
         }
     }
 }
